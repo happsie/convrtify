dbf8a010de3a66d9103d8bb42e2f0146be92785f
happsie <njesper98@gmail.com>
2022-11-08 20:03:51 +0100

    correction for ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/convrtify/',
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
